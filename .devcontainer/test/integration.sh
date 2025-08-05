68be2b93f77b516c3e0268337cecc902404e0dc6
Sergio Hinojosa <sergio.hinojosa@dynatrace.com>
2025-08-05 10:35:27 +0000

    change order so app have time to serve requests
    Signed-off-by: Sergio Hinojosa <sergio.hinojosa@dynatrace.com>

--- .devcontainer/test/integration.sh
+++ .devcontainer/test/integration.sh
@@ -3,13 +3,12 @@ source /workspaces/$RepositoryName/.devcontainer/test/test_functions.sh
 
 printInfoSection "Running integration Tests for the Enablement Framework"
 
-assertRunningApp 30100
-
-assertRunningPod todoapp todoapp
-
 assertRunningPod dynatrace operator
 
 assertRunningPod dynatrace activegate
 
 assertRunningPod dynatrace oneagent
 
+assertRunningPod todoapp todoapp
+
+assertRunningApp 30100
